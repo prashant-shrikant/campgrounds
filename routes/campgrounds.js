feb13af73d4059733daa46317b7e24ab60cd0324
Prashant Shrikant <prashant.shrikant@yahoo.com>
2018-11-25 20:42:22 +0530

    User associated with campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -16,12 +16,16 @@ router.get( '/', function( req, res ) {
 } );
 
 // CREATE - add new campground to DB
-router.post( '/', function( req, res ){
+router.post( '/', isLoggedIn, function( req, res ){
     // get data from form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
     var desc = req.body.description;
-    var newCampground = { name: name, image: image, description: desc }
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = { name: name, image: image, description: desc, author: author };
     // Create a new campground and save to DB
     Campground.create( newCampground, function( err, newlyCreated ){
         if( err ){
@@ -34,7 +38,7 @@ router.post( '/', function( req, res ){
 } );
 
 // NEW - show form to create new campground
-router.get( '/new', function( req, res ) {
+router.get( '/new', isLoggedIn, function( req, res ) {
    res.render( 'campgrounds/new' ); 
 } );
 
@@ -51,5 +55,13 @@ router.get( '/:id', function( req, res ) {
         }
     } );
 } );
+
+function isLoggedIn( req, res, next ) {
+    if( req.isAuthenticated() ) {
+        return next();
+    } else {
+        res.redirect( '/login' );
+    }
+}
 
 module.exports = router;
