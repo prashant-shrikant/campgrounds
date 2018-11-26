424661866389fcaba841cd6cf10ea6d6eafca110
Prashant Shrikant <prashant.shrikant@yahoo.com>
2018-11-26 08:30:19 +0530

    Added campground edit and update functionality

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -56,6 +56,28 @@ router.get( '/:id', function( req, res ) {
     } );
 } );
 
+// EDIT - shows the form to edit the campground
+router.get( '/:id/edit', function( req, res ) { 
+    Campground.findById( req.params.id, function( err, foundCampground ) {
+        if ( err ) {
+            res.redirect( '/campgrounds' );
+        } else {
+            res.render( 'campgrounds/edit', { campground: foundCampground } );
+        }
+    } );
+} );
+
+// UPDATE - route to submit the edit form
+router.put( '/:id', function( req, res ) {
+    Campground.findByIdAndUpdate( req.params.id, req.body.campground, function( err, updatedCampground ) {
+        if ( err ) {
+            res.redirect( '/campgrounds' );
+        } else {
+            res.redirect( '/campgrounds/' + req.params.id );
+        }
+    } );
+} );
+
 function isLoggedIn( req, res, next ) {
     if( req.isAuthenticated() ) {
         return next();
