191a448ab0f42e14abb46c3a085c2dcf2896ff73
Prashant Shrikant <prashant.shrikant@yahoo.com>
2018-11-27 15:17:02 +0530

    Fixed PORT

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var commentRoutes       = require( './routes/comments' ),
     campgroundRoutes    = require( './routes/campgrounds' ),
     indexRoutes         = require( './routes/index' );
 
-    port = process.env.POST || 8080;
+    port = process.env.PORT || 8080;
 
     
 // mongoose.connect( 'mongodb://localhost/yelp_camp_v3' );
