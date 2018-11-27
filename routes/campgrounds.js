329fd8c76f791cb28ae284d4a1481f96994a669f
Prashant Shrikant <prashant.shrikant@yahoo.com>
2018-11-27 07:54:42 +0530

    Feature to input price of each campground

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -19,19 +19,22 @@ router.get( '/', function( req, res ) {
 router.post( '/', middleware.isLoggedIn, function( req, res ){
     // get data from form and add to campgrounds array
     var name = req.body.name;
+    var price = req.body.price;
     var image = req.body.image;
     var desc = req.body.description;
     var author = {
         id: req.user._id,
         username: req.user.username
     }
-    var newCampground = { name: name, image: image, description: desc, author: author };
+    var newCampground = { name: name, price: price, image: image, description: desc, author: author };
+
     // Create a new campground and save to DB
     Campground.create( newCampground, function( err, newlyCreated ){
         if( err ){
             console.log( err );
         } else {
             //redirect back to campgrounds page
+            console.log( newlyCreated );
             res.redirect( '/campgrounds' );
         }
     } );
